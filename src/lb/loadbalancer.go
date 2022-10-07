d594838cd2aae45a8dd5d6f6217ba15978513c45
Gustavo Rodrigues Lima <83222330+GustavoRodrigueess@users.noreply.github.com>
2022-10-07 20:43:08 -0300

    LoadBalancer Logic

---
+++ src/lb/loadbalancer.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Load Balancer")
+
+	machinex := 1
+	machiney := 0
+
+	for {
+
+		if machinex > machiney {
+
+			if machinex >= 11 {
+
+				machinex = 1
+				calculox := ((machinex * 100) / 10)
+				fmt.Println("Máquina X sobrecarregada - Porcentagem: ", calculox, "%")
+				machiney = machiney + 1
+
+			} else {
+
+				calculox := ((machinex * 100) / 10)
+				fmt.Println("Máquina X Sobrecarregada - Porcentagem: ", calculox, "%")
+				machiney = machiney + 1
+
+			}
+
+		} else {
+
+			if machiney >= 11 {
+
+				machiney = 1
+				calculoy := ((machiney * 100) / 10)
+				fmt.Println("Máquina Y Sobrecarregada - Porcentagem: ", calculoy, "%")
+				machinex = machinex + 1
+
+			} else {
+
+				calculoy := ((machiney * 100) / 10)
+				fmt.Println("Máquina Y Sobrecarregada - Porcentagem: ", calculoy, "%")
+				machinex = machinex + 1
+
+			}
+		}
+	}
+}
